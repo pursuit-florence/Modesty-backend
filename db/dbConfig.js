35e73868a75b64c1484d36abe2fc5a90403a1ee9
pursuit-florence <florenceosei@pursuit.org>
2023-05-23 15:58:26 -0400

    backend-configured

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,13 +1,20 @@
-const pgp = require('pg-promise')();
+const pgp = require("pg-promise");
 require("dotenv").config();
 
-const cn = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-}; 
+const { DATABASE_URL } = process.env;
 
-const db = pgp(cn);
+const cn = DATABASE_URL
+    ? {
+        connectionString: DATABASE_URL,
+        max: 30
+    }
+    : {
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        user: process.env.PG_USER
+    };
+
+const db = pgp()(cn);
 
 module.exports = db;
--- db/seed.sql
+++ db/seed.sql
@@ -8,7 +8,7 @@ VALUES
 -- Seed data for the products table
 INSERT INTO products (name, description, price, color, size, image_url, type, category_id)
 VALUES
-  -- Category 1: Women
+  
   ('Classic White T-shirt', 'A basic white dashiki shirt for everyday wear', 349.99, 'White', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/9/optimized/24170659_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'T-shirt', 1),
   ('Belted brownish flower Dress', 'A beautiful belted flowery  dress for special occasions', 79.99, 'Multi-color', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/9/optimized/24499589_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Dress', 1),
   ('Statement Necklace', 'A stunning statement necklace to elevate your outfits', 269.99, 'Gold', 'N/A', 'https://slimages.macysassets.com/is/image/MCY/products/1/optimized/24230611_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Accessory', 1),
@@ -20,7 +20,7 @@ VALUES
   ('Embellished Satin Gown', 'An elegant satin evening gown for formal events', 149.99, 'Mocha', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/0/optimized/18046040_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Dress', 1),
   ('Wide-Brim Straw Hat', 'A fashionable wide-brim straw hat for sun protection', 79.99, 'Caramel', 'N/A', 'https://slimages.macysassets.com/is/image/MCY/products/0/optimized/21753780_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Accessory', 1),
 
-  -- Category 2: Men
+ 
   ('Casual Men Sneakers', 'comfortable and casual sneakers for everyday use', 99.99, 'Black', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/2/optimized/23681512_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp','Shoe', 2),
   ('Classic White Shirt', 'A classic white shirt for formal and casual occasions', 119.99, 'White', 'L', 'https://slimages.macysassets.com/is/image/MCY/products/3/optimized/10789153_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Shirt', 2),
   ('Leather Biker Jacket', 'A fashionable leather biker jacket for a cool look', 129.99, 'Yellow', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/9/optimized/16551659_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Jacket', 2),
@@ -31,7 +31,6 @@ VALUES
   ('Men Tommy Logo Sweatshort', 'Comfortable and breathable cotton shorts for summer', 34.99, 'White', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/3/optimized/24120753_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Shorts', 2),
   ('Slim Fit African Print Pants', 'Stylish and slim-fit pants for a trendy look', 49.99, 'Blue', 'M', 'https://litb-cgis.rightinthebox.com/webp_desc_image/202302/bps/desc/inc/wmyfye1676361485771.jpg?com=1&fmt=webp&v=1', 'Pants', 2),
 
-  -- Category 3: Kids
   ('Cute Animal Print T-shirt', 'A cute t-shirt with animal print for kids', 14.99, 'Navy Blue', 'S', 'https://slimages.macysassets.com/is/image/MCY/products/0/optimized/23225970_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'T-shirt', 3),
   ('Rain Set Dusty Pink & Grey - Toddler|Child', 'A colorful raincoat for protection from rain', 29.99, 'Multi', 'M', 'https://slimages.macysassets.com/is/image/MCY/products/0/optimized/23701850_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Coat', 3),
   ('Kids Denim Overalls', 'Adorable denim overalls for a trendy and playful look', 39.99, 'Light Blue', 'S', 'https://slimages.macysassets.com/is/image/MCY/products/4/optimized/23948984_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1&fmt=webp', 'Overalls', 3),
